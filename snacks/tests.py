d374f6a3a4e08c2c88e30f5f881947f49946d3f9
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-25 10:13:09 -0700

    added tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Snack
+from django.contrib.auth import get_user_model
+
+def setUp(self):
+        auditor = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="snack", purchaser=auditor, description="description")
 
 # Create your tests here.
+
+class SnacksTests(TestCase):
+  def test_list_page_status_code(self):
+    url = reverse("snack_list")
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
